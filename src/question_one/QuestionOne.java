a03493e5404279bd267d56b8a48b48adb608e78f
rylieplatt <rkp6174@rit.edu>
2023-11-08 09:32:16 -0500

    created and implemented QuestionOne function in QuestionOne.java

--- src/question_one/QuestionOne.java
+++ src/question_one/QuestionOne.java
@@ -1,4 +1,31 @@
 package question_one;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class QuestionOne {
+    private final ArrayList<Elf> elves;
+    public QuestionOne(String filename){
+        this.elves = new ArrayList<>();
+        try (Scanner in = new Scanner(new File(filename))) {
+            ArrayList<Integer> food = new ArrayList<>();
+            while (in.hasNextLine()) {
+                String s = in.nextLine();
+                if(s.equals("")){
+                    Elf e = new Elf(food);
+                    elves.add(e);
+                    food = new ArrayList<>();
+                }else{
+                    food.add(Integer.parseInt(s));
+                }
+            }
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("Error opening input file: " + filename);
+        }
+    }
+
+    public static void main(String[] args){
+    }
 }
