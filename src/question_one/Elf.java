00829041b220b46297eb3b2f979ef21042fac387
rylieplatt <rkp6174@rit.edu>
2023-11-09 11:25:39 -0500

    Added documentation

--- src/question_one/Elf.java
+++ src/question_one/Elf.java
@@ -2,16 +2,31 @@ package question_one;
 
 import java.util.ArrayList;
 
+/**
+ * A class representing each elf and how much food they are carrying
+ * @author rylieplatt
+ */
 public class Elf {
+    /** food carried by the elf */
     private final ArrayList<Integer> bag;
 
+    /**
+     * A constructor which makes an elf and fills its bag with
+     * the given list of food
+     *
+     * @param food an arraylist of calories representing food items
+     */
     protected Elf(ArrayList<Integer> food){
         this.bag = food;
     }
 
+    /**
+     * Returns the total calories being carried by the elf
+     * @return total calories being carried by the elf
+     */
     public int getCalories() {
         int calories = 0;
-        for(int food:bag){
+        for(int food:bag){  //iterate through food list to add up cals
             calories += food;
         }
         return calories;
--- src/question_one/QuestionOne.java
+++ src/question_one/QuestionOne.java
@@ -5,19 +5,31 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * This program finds which elf is carrying the most calories given an input
+ * file containing a list of calories
+ *
+ * @author Rylie Platt
+ */
 public class QuestionOne {
+    /** List of elves */
     private final ArrayList<Elf> elves;
+
+    /**
+     * A constructor which makes the list of elves and fills their bags
+     * @param filename string: name of the input file
+     */
     public QuestionOne(String filename){
         this.elves = new ArrayList<>();
         try (Scanner in = new Scanner(new File(filename))) {
             ArrayList<Integer> food = new ArrayList<>();
             while (in.hasNextLine()) {
                 String s = in.nextLine();
-                if(s.equals("")){
+                if(s.equals("")){   //everytime there is an empty line make a new elf with collected cals
                     Elf e = new Elf(food);
                     elves.add(e);
-                    food = new ArrayList<>();
-                }else{
+                    food = new ArrayList<>();   //empty food list
+                }else{  //if line is not empty add cal count to the list
                     food.add(Integer.parseInt(s));
                 }
             }
@@ -26,15 +38,26 @@ public class QuestionOne {
         }
     }
 
+    /**
+     * Returns the total calories carried by elf with the most calories
+     * @param elf Arraylist containing all elves
+     * @return total calories carried by elf with the most calories
+     */
     private int getBiggest(ArrayList<Elf> elf){
         int mostCals = 0;
-        for(Elf e:elf){
+        for(Elf e:elf){ //iterate through elves to find the one with the most total cals
             if (mostCals <= e.getCalories()){
                 mostCals = e.getCalories();
             }
         }
         return mostCals;
     }
+
+    /**
+     * Creates the list of elves and prints the value of the largest amount
+     * of calories carried by the elf with the most calories.
+     * @param args command line arguments (the filename)
+     */
     public static void main(String[] args){
         QuestionOne qOne = new QuestionOne(args[0]);
         System.out.println(qOne.getBiggest(qOne.elves));
